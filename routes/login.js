95b697993636a47d5200059bc8db102864d8d69c
unknown <jnip2@my.bcit.ca>
2022-03-06 19:41:24 -0800

    added POST to connect login/signup to sql, debugged databaseAccess

--- routes/login.js
+++ routes/login.js
@@ -2,20 +2,52 @@ const express = require('express')
 const router = express.Router();
 const bodyParser = require("body-parser")
 const database = require("../databaseConnection");
+const db = require("../databaseAccess")
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(express.static("public"))
 
 const getSession = (session) => {
   if (session.username) {
-      return session;
+    return session;
   } else {
-      return null;
+    return null;
   }
 }
 
 router.get("/", (req, res) => {
-    res.render('loginForm')
+  res.render('loginForm')
+})
+
+router.post("/", (req, res) => {
+  database.getConnection(function (err, dbConnection) {
+    if (err) {
+      res.render('error', { message: 'Error connecting to MySQL' });
+      console.log("Error connecting to mysql");
+      console.log(err);
+    } else {
+      db.getUserByLogin(req.body, (err, result) => {
+        if (err) {
+          res.render('error', { message: 'Error reading from MySQL' });
+          console.log("Error reading from mysql");
+          console.log(err);
+        }
+        else { //success
+          console.log(req.body)
+          /*
+          rn everyone gets redirected to lobby
+          need session to verify user is signed in
+          */
+          if (req.body) {
+            res.redirect('/lobby');
+          } else {
+            res.redirect('/')
+          }
+        }
+      })
+      dbConnection.release();
+    }
   })
+})
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const bodyParser = require("body-parser")
 const database = require("../databaseConnection");
+const db = require("../databaseAccess")
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(express.static("public"))
@@ -17,5 +18,28 @@ const getSession = (session) => {
 router.get("/", (req, res) => {
     res.render('signUpForm')
   })
+
+router.post("/", (req, res) => {
+  database.getConnection(function (err, dbConnection) {
+    if (err) {
+			res.render('error', { message: 'Error connecting to MySQL' });
+			console.log("Error connecting to mysql");
+			console.log(err);
+		} else {
+      db.addUser(req.body, (err, result) => {
+				if (err) {
+					res.render('error', { message: 'Error reading from MySQL' });
+					console.log("Error reading from mysql");
+					console.log(err);
+				}
+				else { //success
+					res.redirect('/');
+          console.log('account created');
+				}
+      })
+      dbConnection.release();
+    }
+  })
+})
 
 module.exports = router;
