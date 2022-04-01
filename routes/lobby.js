0c1164d12769eb7742a8dc8fb16e24c384d3495b
Callista Stefanie Taswin <callisto@Callistas-MacBook-Air.local>
2022-03-31 19:36:51 -0700

    fixed it a tad bit

--- routes/lobby.js
+++ routes/lobby.js
@@ -6,21 +6,21 @@ const database = require("../databaseConnection");
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(express.static("public"))
 
-// const getSession = (session) => {
-//     if (session.user_name) {
-//         return session;
-//     } else {
-//         return null;
-//     }
-// }
+const getSession = (session) => {
+    if (session.user_name) {
+        return session;
+    } else {
+        return null;
+    }
+}
 
 router.get("/", (req, res) => {
-    // let user = getSession(req.session)
-    // if (user) {
-        res.status(200).render("lobby");
-    // } else {
-    //     res.status(300).redirect('/');
-    // }
+    let user = getSession(req.session)
+    if (user) {
+        res.status(200).render("lobby", { user });
+    } else {
+        res.status(300).redirect('/');
+    }
 })
 
 module.exports = router;
