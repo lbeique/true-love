fd03a3594320c9d59953954cdea0602ae0471109
Laurent Beique <lbeique@outlook.com>
2022-05-13 07:18:49 -0700

    welcome screen TEMPORARY login

--- routes/auth.js
+++ routes/auth.js
@@ -15,50 +15,76 @@ const getSession = (session) => {
 }
 
 
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
 
+  let username = req.body.username
   ////////////////////////////////////////////////////////
   // THIS IS TEMPORARY => (
   req.session.authenticated = true;
   req.session.user_info = {}
-  req.session.user_info.user_name = makeUsername()
-  req.session.user_info.user_id = Math.floor(Math.random() * 1000)
-  req.session.user_info.total_points = Math.floor(Math.random() * 2)
+  req.session.user_info.user_name = username
+  req.session.user_info.user_id = Math.floor(Math.random() * 9000)
+  req.session.user_info.total_points = 0
   // )
   ///////////////////////////////////////////////////////
 
   const session = getSession(req.session)
   console.log('get login session', session)
   if (!session) {
-    res.status(200).render('loginForm')
+    res.status(200).redirect('/')
     return
   }
   res.status(200).redirect('/mainmenu')
   return
 })
 
-router.post("/login", async (req, res) => {
-  const session = getSession(req.session)
-  console.log('post login session', session)
-  if (!session) {
-    const user_info = await database.getUserByLogin(req.body)
-    console.log('route', user_info)
-    if (!user_info) {
-      console.log('Error with login')
-      res.status(404).redirect('/auth/login')
-      return
-    }
-    req.session.user_info = {}
-    req.session.user_info.user_name = user_info.user_name
-    req.session.user_info.user_id = +user_info.user_id
-    req.session.user_info.total_points = +user_info.total_points
-    res.status(200).redirect('/')
-    return
-  }
-  console.log('Error with login')
-  res.status(404).redirect('/')
-  return
-})
+// TEMPORARY
+// router.get("/login", (req, res) => {
+
+//   ////////////////////////////////////////////////////////
+//   // THIS IS TEMPORARY => (
+//   req.session.authenticated = true;
+//   req.session.user_info = {}
+//   req.session.user_info.user_name = makeUsername()
+//   req.session.user_info.user_id = Math.floor(Math.random() * 1000)
+//   req.session.user_info.total_points = Math.floor(Math.random() * 2)
+//   // )
+//   ///////////////////////////////////////////////////////
+
+//   const session = getSession(req.session)
+//   console.log('get login session', session)
+//   if (!session) {
+//     res.status(200).render('loginForm')
+//     return
+//   }
+//   res.status(200).redirect('/mainmenu')
+//   return
+// })
+
+
+// TEMPORARY
+// router.post("/login", async (req, res) => {
+//   const session = getSession(req.session)
+//   console.log('post login session', session)
+//   if (!session) {
+//     const user_info = await database.getUserByLogin(req.body)
+//     console.log('route', user_info)
+//     if (!user_info) {
+//       console.log('Error with login')
+//       res.status(404).redirect('/auth/login')
+//       return
+//     }
+//     req.session.user_info = {}
+//     req.session.user_info.user_name = user_info.user_name
+//     req.session.user_info.user_id = +user_info.user_id
+//     req.session.user_info.total_points = +user_info.total_points
+//     res.status(200).redirect('/')
+//     return
+//   }
+//   console.log('Error with login')
+//   res.status(404).redirect('/')
+//   return
+// })
 
 router.get("/signup", (req, res) => {
   const session = getSession(req.session)
